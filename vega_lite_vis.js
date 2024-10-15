ccae7edb92847043eb5b0bd174a3f00acce44ef6
gxting04 <99959404+gxting04@users.noreply.github.com>
2024-10-15 22:25:48 +0800

    Add files for A2

---
+++ vega_lite_vis.js
@@ -0,0 +1,66 @@
+var vg1 = 'map.json';
+var vg2 = 'pyramid.json';
+var vg3 = 'ph.json';
+var vg4 = 'coalition.json';
+var vg5 = 'map_governing_party.json';
+var vg6 = 'ethnicity.json';
+var vg7 = 'majority.json';
+var vg8 = 'bn.json';
+var vg9 = 'pn.json';
+var vg10 = 'gps.json';
+var vg11 = 'grs.json';
+
+vegaEmbed("#map", vg1).then(function(result){
+    // Access the Vega view instance 
+    // https://vega.github.io/vega/docs/api/view/ as result.view
+}).catch(console.error)
+
+vegaEmbed("#pyramid", vg2).then(function(result){
+    // Access the Vega view instance 
+    // https://vega.github.io/vega/docs/api/view/ as result.view
+}).catch(console.error)
+
+vegaEmbed("#ph", vg3).then(function(result){
+    // Access the Vega view instance 
+    // https://vega.github.io/vega/docs/api/view/ as result.view
+}).catch(console.error)
+
+vegaEmbed("#coalition", vg4).then(function(result){
+    // Access the Vega view instance 
+    // https://vega.github.io/vega/docs/api/view/ as result.view
+}).catch(console.error)
+
+vegaEmbed("#govern", vg5).then(function(result){
+    // Access the Vega view instance 
+    // https://vega.github.io/vega/docs/api/view/ as result.view
+}).catch(console.error)
+
+vegaEmbed("#ethnicity", vg6).then(function(result){
+    // Access the Vega view instance 
+    // https://vega.github.io/vega/docs/api/view/ as result.view
+}).catch(console.error)
+
+vegaEmbed("#majority", vg7).then(function(result){
+    // Access the Vega view instance 
+    // https://vega.github.io/vega/docs/api/view/ as result.view
+}).catch(console.error)
+
+vegaEmbed("#bn", vg8).then(function(result){
+    // Access the Vega view instance 
+    // https://vega.github.io/vega/docs/api/view/ as result.view
+}).catch(console.error)
+
+vegaEmbed("#pn", vg9).then(function(result){
+    // Access the Vega view instance 
+    // https://vega.github.io/vega/docs/api/view/ as result.view
+}).catch(console.error)
+
+vegaEmbed("#gps", vg10).then(function(result){
+    // Access the Vega view instance 
+    // https://vega.github.io/vega/docs/api/view/ as result.view
+}).catch(console.error)
+
+vegaEmbed("#grs", vg11).then(function(result){
+    // Access the Vega view instance 
+    // https://vega.github.io/vega/docs/api/view/ as result.view
+}).catch(console.error)
